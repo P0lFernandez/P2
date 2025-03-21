809741349f3f9cc32a6c74056ad88a0f11f56eeb
P0lFernandez <pol.fernandez.martinez@estudiantat.upc.edu>
2025-03-21 09:09:35 +0100

    cambios vad

--- src/vad.c
+++ src/vad.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "vad.h"
+#include "pav_analysis.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
 
@@ -42,12 +43,14 @@ Features compute_features(const float *x, int N) {
    * For the moment, compute random value between 0 and 1 
    */
   Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  //feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  feat.p = compute_power(x,N);
   return feat;
 }
 
 /* 
  * TODO: Init the values of vad_data
+
  */
 
 VAD_DATA * vad_open(float rate) {
